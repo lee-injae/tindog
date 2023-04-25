6b7344a375935d83bef14a6346bd44131c6d5dca
Injae Lee <ilee.injae@gmail.com>
2023-04-25 15:13:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+document.getElementById("like-icon").addEventListener("click", function(e){
+    console.log(e.target)
+    
+})
+
+
+
+// document.addEventListener("click", function(e){
+//     console.log(e.target)
+// })
